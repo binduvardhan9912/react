ba2012541f1ec5f70591775db0a3cecbaacea0c2
Bindu Vardhan <binduvardhan9912@gmail.com>
2023-11-24 16:33:43 +0530

    message

---
+++ movie/src/App.js
@@ -0,0 +1,63 @@
+import './App.css'
+function Cinema (){
+  return(
+    <div className="main">
+    <div>
+       Movie Name : SHIVA<br></br>
+       Status : Industry hit
+    </div>
+    <div>
+        Movie Name : Rangeela<br></br>
+        Status : Superhit
+      </div>
+      <div>
+        Movie Name : Company<br></br>
+        Status : Blockbuster
+      </div>
+      <div>
+        Movie Name : Satya<br></br>
+        Status : Hit
+      </div>
+      <div>
+        Movie Name : Raktha Charitra<br></br>
+        Status : Hit
+      </div>
+      <div>
+        Movie Name : Sarkar<br></br>
+        Status : Superhit
+      </div>
+      <div>
+        Movie Name :Pokiri<br></br>
+        Status : Industry hit
+      </div>
+      <div>
+        Movie Name :Businessman<br></br>
+        Status : MASSive cult
+      </div>
+      <div>
+        Movie Name :Desamuduru<br></br>
+        Status : Box-officehit
+      </div>
+      <div>
+        Movie Name :Pushpa<br></br>
+        Status :PAN Indian Blockbuster
+      </div>
+      <div>
+        Movie Name :KGF<br></br>
+        Status :PAN Indian Blockbuster
+      </div>
+      <div>
+        Movie Name :Bahubali<br></br>
+        Status :PAN Indian Blockbuster
+      </div>
+
+      </div>
+      
+
+    
+    
+  )
+}
+export default Cinema;
+
+
---
+++ movie/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Cinema from "./App";
+import './index.css'
+
+
+const root=ReactDOM.createRoot(document.getElementById ("root"))
+root.render(
+  <div>
+    <h1>MOVIE'S</h1>
+    <Cinema />
+  </div>
+  
+)
